f9882923cc6bd2b604f81ababfed86a68756e234
elena_iankovskaia <elena.parokhina@gmail.com>
2025-10-20 22:36:43 +0200

    refactoring ms-orders

--- orders/src/main/java/ru/pro/service/impl/OrderServiceImpl.java
+++ orders/src/main/java/ru/pro/service/impl/OrderServiceImpl.java
@@ -1,7 +1,7 @@
 package ru.pro.service.impl;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.pro.kafka.OrderProducer;
 import ru.pro.mapper.OrderMapper;
@@ -16,24 +16,23 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderServiceImpl implements OrderService {
     private final OrderProducer producer;
     private final OrderMapper mapper;
     private final Map<String, OrderDto> storage = new ConcurrentHashMap<>();
 
-    @Value("${spring.kafka.producer.group-id")
-    private String groupId;
-
     @Override
     public OrderDto createOrder(OrderCreateDto dto) {
         OrderDto order = mapper.fromCreateDto(dto);
         storage.put(order.id(), order);
-        producer.sendNewOrder(groupId, order.id(), dto);
+        producer.sendNewOrder("new_orders", order.id(), dto);
         return order;
     }
 
     @Override
     public OrderDto updateStatus(String orderId, OrderStatus status) {
+        log.info("orderId: {}, status {}: ", orderId, status);
         OrderDto existing = storage.get(orderId);
         if (existing == null) {
             throw new NoSuchElementException("Order not found: " + orderId);
